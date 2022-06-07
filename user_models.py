4017adcef4594cf296db09f85ee2186779d8eb0d
rahammo <97483934+rahammo@users.noreply.github.com>
2022-06-07 15:01:42 -0500

    Update user_models.py

--- user_models.py
+++ user_models.py
@@ -1,4 +1,3 @@
-import sqlite3
 from db import db
 
 class UserModel(db.Model):
@@ -8,39 +7,18 @@ class UserModel(db.Model):
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
     
-    def __init__(self, _id, username, password):
-        self.id = _id
+    def __init__(self, username, password):
         self.username = username
         self.password = password
+        
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
 
     @classmethod
     def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = 'SELECT * FROM users WHERE username=?'
-        result = cursor.execute(query, (username,)) # (variable, ) <-- this is used to create a tuple
-        row = result.fetchone() # gets the first row out of the result set
-        if row: # this is creating a user object that will create a column/attribute specified in the __init__ method
-            user = cls(*row) # ([0], row[1], row[2]) this matches the parameters from the __init__ method, but is the same as (*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(username=username).first()
 
     @classmethod 
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = 'SELECT * FROM users WHERE id=?'
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(id=_id).first()
